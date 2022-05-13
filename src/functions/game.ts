e6dcd6d50004a038e517daa626ee648b26edd69e
Brian Gradin <brian@toolhouse.com>
2022-05-12 17:52:48 -0700

    Adds board visualization and message swapping

--- src/functions/game.ts
+++ src/functions/game.ts
@@ -1,16 +1,47 @@
-import Discord from "discord.js";
+import Discord, { ThreadChannel } from "discord.js";
 import { Chess } from "chess.ts";
+import ChessImageGenerator from "chess-image-generator";
 import client from "../client";
 import { Command, DEFAULT_COMMAND_TYPE } from "../commands";
 import Function from "./function";
 
 const USER_MENTION_RX = /<@(\d+)>/;
 
+const Commands = {
+  CHALLENGE: "challenge",
+  MOVES: "moves",
+};
+
+interface GameInfo {
+  chess: Chess;
+  previousMessage?: Discord.Message;
+}
+
 interface GameStore {
-  [key: string]: Chess;
+  [key: string]: GameInfo;
 }
 
 const games: GameStore = {};
+const imageGenerator = new ChessImageGenerator();
+
+async function sendGameState(channel: Discord.ThreadChannel, info: GameInfo) {
+  imageGenerator.loadFEN(info.chess.fen());
+  const png = await imageGenerator.generateBuffer();
+
+  const payload = {
+    files: [
+      {
+        attachment: png,
+      },
+    ],
+  };
+
+  if (info.previousMessage) {
+    await info.previousMessage.delete();
+  }
+
+  info.previousMessage = await channel.send(payload);
+}
 
 async function getUser(userId: string): Promise<Discord.User> {
   try {
@@ -22,20 +53,27 @@ async function getUser(userId: string): Promise<Discord.User> {
 
 export default class GameFunction implements Function {
   canExecute(command: Command): boolean {
+    // Valid guild commands
     if (
-      command.type === "challenge" &&
+      command.type === Commands.CHALLENGE &&
       ["GUILD_TEXT", "GUILD_NEWS"].includes(command.message.channel.type)
     ) {
       return true;
     }
 
+    // Valid thread commands
     if (
-      command.type === DEFAULT_COMMAND_TYPE &&
       ["GUILD_PUBLIC_THREAD", "GUILD_PRIVATE_THREAD"].includes(
         command.message.channel.type
       )
     ) {
-      return true;
+      if (command.type === DEFAULT_COMMAND_TYPE) {
+        return true;
+      }
+
+      if (command.type === Commands.MOVES) {
+        return true;
+      }
     }
 
     return false;
@@ -47,7 +85,11 @@ export default class GameFunction implements Function {
         command.message.channel.type
       )
     ) {
-      await this.handleMove(command);
+      if (command.type === DEFAULT_COMMAND_TYPE) {
+        await this.handleMove(command);
+      } else if (command.type === Commands.MOVES) {
+        await this.printMoves(command);
+      }
     } else {
       await this.startNewGame(command);
     }
@@ -68,25 +110,39 @@ export default class GameFunction implements Function {
       name: `${command.message.author.username} vs ${targetUser.username}`,
     });
 
-    const game = new Chess();
-    games[thread.id] = game;
+    const info: GameInfo = {
+      chess: new Chess(),
+    };
+    games[thread.id] = info;
 
-    thread.send(`\`\`\`${game.ascii()}\`\`\``);
+    await sendGameState(thread, info);
   }
 
   async handleMove(command: Command) {
-    const game = games[command.message.channel.id];
-    if (!game) {
+    const info = games[command.message.channel.id];
+    if (!info) {
       return;
     }
 
     const move = command.args[0].trim();
-    const moves: string[] = game.moves();
+    const moves: string[] = info.chess.moves();
     if (moves.includes(move)) {
-      game.move(move);
-      command.message.channel.send(`\`\`\`${game.ascii()}\`\`\``);
+      info.chess.move(move);
+
+      await sendGameState(command.message.channel as ThreadChannel, info);
     } else {
-      command.message.channel.send("Invalid move!");
+      command.message.channel.send(
+        'Invalid move! If you get stuck, try "!moves"'
+      );
     }
+  }
+
+  async printMoves(command: Command) {
+    const info = games[command.message.channel.id];
+    if (!info) {
+      return;
+    }
+
+    command.message.channel.send(info.chess.moves().join(", "));
   }
 }
