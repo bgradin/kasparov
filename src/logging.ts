f31871450137145ecb0d43efba1cc35a0d62c954
Brian Gradin <brian@toolhouse.com>
2022-05-13 18:23:18 -0700

    Adds time stamp to error messages

--- src/logging.ts
+++ src/logging.ts
@@ -3,8 +3,20 @@ import { appendfile } from "./utils";
 
 export const ERROR_LOG_PATH = path.join(process.cwd(), "error.log");
 
+function getTimestamp() {
+  const pad = (n, s = 2) => `${new Array(s).fill(0)}${n}`.slice(-s);
+  const d = new Date();
+
+  return `${pad(d.getFullYear(), 4)}-${pad(d.getMonth() + 1)}-${pad(
+    d.getDate()
+  )} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+}
+
 async function error(err: Error) {
-  await appendfile(ERROR_LOG_PATH, `Exception: ${err.message}\n${err.stack}`);
+  await appendfile(
+    ERROR_LOG_PATH,
+    `${getTimestamp} -- ${err.message}\n${err.stack}`
+  );
 }
 
 export default {
