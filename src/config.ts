b082fe407382705ed4c34462e18ef2f6d77bce87
Brian Gradin <brian@toolhouse.com>
2022-06-12 12:08:22 -0700

    Adds rules handler config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 import GameHandler from "./handlers/game/handler";
 import HelpHandler from "./handlers/help";
 import PuzzleHandler from "./handlers/puzzle/handler";
+import RulesHandler from "./handlers/rules";
 
 export const Handlers = {
-  ALL: [new GameHandler(), new PuzzleHandler(), new HelpHandler()],
+  ALL: [new GameHandler(), new PuzzleHandler(), new HelpHandler(), new RulesHandler()],
 };
