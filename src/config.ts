dd1a57ec29954a05a3f4f1e7923d4fc1dfbb2318
Brian Gradin <brian@toolhouse.com>
2024-08-08 10:01:09 -0700

    Hooks up status handler

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,14 @@ import GameHandler from "./handlers/game/handler";
 import HelpHandler from "./handlers/help";
 import PuzzleHandler from "./handlers/puzzle/handler";
 import RulesHandler from "./handlers/rules";
+import StatusHandler from "./handlers/status/handler";
 
 export const Handlers = {
-  ALL: [new GameHandler(), new PuzzleHandler(), new HelpHandler(), new RulesHandler()],
+  ALL: [
+    new GameHandler(),
+    new PuzzleHandler(),
+    new HelpHandler(),
+    new RulesHandler(),
+    new StatusHandler(),
+  ],
 };
